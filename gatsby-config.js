e1972c6389cefdbde7c61f641b3f641a3ff1867d
prasad.jivane <prasad.jivane@aitglobalinc.com>
2021-11-13 23:49:20 +0530

    My Gatsbay Website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
+    title: "My Gatsbay Site",
   },
   plugins: [
     "gatsby-plugin-image",
